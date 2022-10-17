cbc819a0b039596dff79ea1d5244cc24de054a37
florian.scheidl@gmail.com <florian.scheidl@gmail.com>
2022-10-17 12:28:38 +0200

    Added general 1-2-3 gnn class, parser for keyword arguments and loader for datasets.

---
+++ examples/1-2-3-gnn-class.py
@@ -0,0 +1,272 @@
+import os.path as osp
+
+import wandb
+import argparse
+import numpy as np
+import random
+import torch
+import torch.nn.functional as F
+from torch_scatter import scatter_mean
+from torch_geometric.datasets import TUDataset
+from torch_geometric.utils import degree
+import torch_geometric.transforms as T
+
+from k_gnn import DataLoader, GraphConv, avg_pool
+from k_gnn.pyg_loader import load_pyg, load_ogb
+from k_gnn import TwoMalkin, ConnectedThreeMalkin
+from get_parser import get_parser
+
+# Specify all the arguments (that wandb will use later) here.
+parser = get_parser()
+args = parser.parse_args()
+
+# Set the seed for everything
+torch.manual_seed(args.seed)
+torch.cuda.manual_seed(args.seed)
+torch.cuda.manual_seed_all(args.seed)
+np.random.seed(args.seed)
+random.seed(args.seed)
+
+# TODO: We omit the pre_filter!
+class MyFilter(object):
+    def __call__(self, data):
+        return data.num_nodes <= 70
+
+
+class MyPreTransform(object):
+    def __call__(self, data):
+        data.x = torch.zeros((data.num_nodes, 1), dtype=torch.float)
+        data = TwoMalkin()(data)
+        data = ConnectedThreeMalkin()(data)
+        data.x = degree(data.edge_index[0], data.num_nodes, dtype=torch.long)
+        data.x = F.one_hot(data.x, num_classes=136).to(torch.float)
+        return data
+
+
+BATCH = args.batch_size
+path = osp.join(
+    osp.dirname(osp.realpath(__file__)), '..', 'data', '1_2_3_gnn', args.dataset)
+
+
+# load and transform dataset
+pre_transform = T.Compose([MyPreTransform()])
+pre_filter = MyFilter()
+
+if args.data_format == 'PyG':
+    dataset = load_pyg(dataset_dir=path, name=args.dataset, pre_transform=pre_transform)
+elif args.data_format == 'ogb':
+    dataset = load_ogb(dataset_dir=path, name=args.dataset, pre_transform=pre_transform)
+else:
+    raise ValueError('Unknown data format: {}'.format(args.data_format))
+
+
+perm = torch.randperm(len(dataset), dtype=torch.long)
+dataset = dataset[perm]
+
+dataset.data.iso_type_2 = torch.unique(dataset.data.iso_type_2, True, True)[1]
+num_i_2 = dataset.data.iso_type_2.max().item() + 1
+dataset.data.iso_type_2 = F.one_hot(
+    dataset.data.iso_type_2, num_classes=num_i_2).to(torch.float)
+
+dataset.data.iso_type_3 = torch.unique(dataset.data.iso_type_3, True, True)[1]
+num_i_3 = dataset.data.iso_type_3.max().item() + 1
+dataset.data.iso_type_3 = F.one_hot(
+    dataset.data.iso_type_3, num_classes=num_i_3).to(torch.float)
+
+num_i = [args.initial_emb_dim, num_i_2, num_i_3]
+
+class Net(torch.nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+
+        # initial layer
+        setattr(self,
+                'conv_initial',
+                GraphConv(dataset.num_features, args.initial_emb_dim))
+
+        # args.num_layers_per_dim layers per dimension j
+        for j in range(args.max_k):
+            setattr(self,
+                    'conv{}_{}'.format(j, 0),
+                    GraphConv(num_i[j], args.emb_dim))
+            for i in range(1,args.num_layers_per_dim):
+                setattr(self,
+                        'conv{}_{}'.format(j, i),
+                        GraphConv(args.emb_dim, args.emb_dim))
+
+        # final_classification layers
+        setattr(self, 'fc0', torch.nn.Linear(args.max_k*args.emb_dim, args.emb_dim))
+        for l in range(1,args.num_linear_layers-1):
+            setattr(self, 'fc{}'.format(l), torch.nn.Linear(args.emb_dim, args.emb_dim/2))
+        setattr(self, 'fc{}'.format(args.num_linear_layers-1), torch.nn.Linear(max(4,args.emb_dim/(2^(args.max_k-2))), dataset.num_classes))
+
+        # self.conv1 = GraphConv(dataset.num_features, 32)
+        # self.conv2 = GraphConv(32, 64)
+        # self.conv3 = GraphConv(64, 64)
+        # self.conv4 = GraphConv(64 + num_i_2, 64)
+        # self.conv5 = GraphConv(64, 64)
+        # self.conv6 = GraphConv(64 + num_i_3, 64)
+        # self.conv7 = GraphConv(64, 64)
+        # self.fc1 = torch.nn.Linear(3 * 64, 64)
+        # self.fc2 = torch.nn.Linear(64, 32)
+        # self.fc3 = torch.nn.Linear(32, dataset.num_classes)
+
+    def reset_parameters(self):
+        for (name, module) in self._modules.items():
+            module.reset_parameters()
+
+    def forward(self, data):
+        non_linearity = getattr(F, args.nonlinearity)
+        x_per_dim = []
+
+        # initial layer
+        data.x = non_linearity(getattr(self, 'conv_initial')(data.x, data.edge_index))
+
+        for j in range(args.max_k):
+            for i in range(args.num_layers_per_dim):
+                if j==0:
+                    data.x = non_linearity(getattr(self, 'conv{}_{}'.format(j, i))(data.x, data.edge_index))
+                else:
+                    data.x = non_linearity(getattr(self, 'conv{}_{}'.format(j, i))(data.x, getattr(data, 'edge_index_{}'.format(j+1))))
+            x = data.x
+            x_per_dim.append(scatter_mean(data.x, data.batch, dim=0))
+            if j<args.max_k-1:
+                data.x = avg_pool(x, getattr(data, f'assignment_index_{j+2}'))
+                data.x = torch.cat([data.x, getattr(data, f'iso_type_{j+2}')], dim=1)
+            else:
+                x = torch.cat(x_per_dim, dim=1)
+
+        if args.no_train:
+            x = x.detach()
+
+        for l in range(args.num_linear_layers-1):
+            x = non_linearity(getattr(self, 'fc{}'.format(l))(x))
+            if l<args.num_linear_layers-2:
+                x = F.dropout(x, p=args.dropout, training=self.training)
+        x = getattr(self, 'fc{}'.format(args.num_linear_layers-1))(x)
+        return F.log_softmax(x, dim=1)
+
+        # data.x = non_linearity(self.conv1(data.x, data.edge_index))
+        # data.x = non_linearity(self.conv2(data.x, data.edge_index))
+        # data.x = non_linearity(self.conv3(data.x, data.edge_index))
+        # x = data.x
+        # x_1 = scatter_mean(data.x, data.batch, dim=0)
+        #
+        # data.x = avg_pool(x, data.assignment_index_2)
+        # data.x = torch.cat([data.x, data.iso_type_2], dim=1)
+        #
+        # data.x = non_linearity(self.conv4(data.x, data.edge_index_2))
+        # data.x = non_linearity(self.conv5(data.x, data.edge_index_2))
+        # x_2 = scatter_mean(data.x, data.batch_2, dim=0)
+        #
+        # data.x = avg_pool(x, data.assignment_index_3)
+        # data.x = torch.cat([data.x, data.iso_type_3], dim=1)
+        #
+        # data.x = non_linearity(self.conv6(data.x, data.edge_index_3))
+        # data.x = non_linearity(self.conv7(data.x, data.edge_index_3))
+        # x_3 = scatter_mean(data.x, data.batch_3, dim=0)
+        #
+        # x = torch.cat([x_1, x_2, x_3], dim=1)
+        #
+        # if args.no_train:
+        #     x = x.detach()
+        #
+        # x = non_linearity(self.fc1(x))
+        # x = F.dropout(x, p=args.drop_rate, training=self.training)
+        # x = non_linearity(self.fc2(x))
+        # x = self.fc3(x)
+        # return F.log_softmax(x, dim=1)
+
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+model = Net().to(device)
+
+
+def train(epoch, loader, optimizer):
+    model.train()
+    loss_all = 0
+
+    for data in loader:
+        data = data.to(device)
+        optimizer.zero_grad()
+        loss = F.nll_loss(model(data), data.y)
+        loss.backward()
+        loss_all += data.num_graphs * loss.item()
+        optimizer.step()
+    return loss_all / len(loader.dataset)
+
+
+def val(loader):
+    model.eval()
+    loss_all = 0
+
+    for data in loader:
+        data = data.to(device)
+        loss_all += F.nll_loss(model(data), data.y, reduction='sum').item()
+    return loss_all / len(loader.dataset)
+
+
+def test(loader):
+    model.eval()
+    correct = 0
+
+    for data in loader:
+        data = data.to(device)
+        pred = model(data).max(1)[1]
+        correct += pred.eq(data.y).sum().item()
+    return correct / len(loader.dataset)
+
+
+acc = []
+for i in range(args.folds):
+    model.reset_parameters()
+    optimizer = torch.optim.Adam(model.parameters(), lr=args.lr)
+    scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(
+        optimizer, mode='min', factor=args.lr_scheduler_decay_rate, patience=args.lr_scheduler_patience, min_lr=args.lr_scheduler_min)
+
+    if hasattr(dataset, 'test_mask'):
+        test_mask = dataset.test_mask
+    elif hasattr(dataset, 'test_graph_index'):
+        test_mask = dataset.test_graph_index
+    else:
+        test_mask = torch.zeros(len(dataset), dtype=torch.bool)
+        n = len(dataset) // 10
+        test_mask[i * n:(i + 1) * n] = 1
+    test_dataset = dataset[test_mask]
+
+    train_dataset = dataset[~test_mask]
+
+    if hasattr(dataset, 'val_mask'):
+        val_mask = dataset.val_mask
+    elif hasattr(dataset, 'val_graph_index'):
+        val_mask = dataset.val_graph_index
+    else:
+        n = len(train_dataset) // 10
+        val_mask = torch.zeros(len(train_dataset), dtype=torch.bool)
+        val_mask[i * n:(i + 1) * n] = 1
+    val_dataset = train_dataset[val_mask]
+
+    train_dataset = train_dataset[~val_mask]
+
+    val_loader = DataLoader(val_dataset, batch_size=BATCH)
+    test_loader = DataLoader(test_dataset, batch_size=BATCH)
+    train_loader = DataLoader(train_dataset, batch_size=BATCH, shuffle=True)
+
+    print('---------------- Split {} ----------------'.format(i))
+
+    best_val_loss, test_acc = 100, 0
+    for epoch in range(1, args.epochs + 1):
+        lr = scheduler.optimizer.param_groups[0]['lr']
+        train_loss = train(epoch, train_loader, optimizer)
+        val_loss = val(val_loader)
+        scheduler.step(val_loss)
+        if best_val_loss >= val_loss:
+            test_acc = test(test_loader)
+            best_val_loss = val_loss
+        print('Epoch: {:03d}, LR: {:7f}, Train Loss: {:.7f}, '
+              'Val Loss: {:.7f}, Test Acc: {:.7f}'.format(
+                  epoch, lr, train_loss, val_loss, test_acc))
+    acc.append(test_acc)
+acc = torch.tensor(acc)
+print('---------------- Final Result ----------------')
+print('Mean: {:7f}, Std: {:7f}'.format(acc.mean(), acc.std()))
---
+++ examples/get_parser.py
@@ -0,0 +1,83 @@
+import argparse
+import time
+import os
+
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+
+def get_parser():
+    parser = argparse.ArgumentParser(description='k-GNN experiment.')
+    parser.add_argument('--no-train', default=False)
+    parser.add_argument('--seed', type=int, default=43,
+                        help='random seed to set (default: 43, i.e. the non-meaning of life))')
+    # parser.add_argument('--start_seed', type=int, default=0,
+    #                     help='The initial seed when evaluating on multiple seeds.')
+    # parser.add_argument('--stop_seed', type=int, default=9,
+    #                     help='The final seed when evaluating on multiple seeds.')
+    # parser.add_argument('--device', type=int, default=0,
+    #                     help='which gpu to use if any (default: 0)')
+    # parser.add_argument('--model', type=str, default='sparse_cin',
+    #                     help='model, possible choices: cin, dummy, ... (default: cin)')
+    parser.add_argument('--drop_rate', type=float, default=0.0,
+                        help='dropout rate (default: 0.5)')
+    parser.add_argument('--nonlinearity', type=str, default='relu',
+                        help='activation function (default: relu)')
+    # parser.add_argument('--pool_func', type=str, default='sum',
+    #                     help='Pooling function (default: avg_pool)')
+   # parser.add_argument('--graph_norm', type=str, default='bn', choices=['bn', 'ln', 'id'],
+   #                      help='Normalization layer to use inside the model')
+    parser.add_argument('--lr', type=float, default=0.001,
+                        help='learning rate (default: 0.001)')
+    # parser.add_argument('--lr_scheduler', type=str, default='StepLR',
+    #                     help='learning rate decay scheduler (default: StepLR)')
+    # parser.add_argument('--lr_scheduler_decay_steps', type=int, default=50,
+    #                     help='number of epochs between lr decay (default: 50)')
+    parser.add_argument('--lr_scheduler_decay_rate', type=float, default=0.5,
+                        help='strength of lr decay (default: 0.5)')
+    parser.add_argument('--lr_scheduler_patience', type=float, default=5,
+                        help='patience for `ReduceLROnPlateau` lr decay (default: 10)')
+    parser.add_argument('--lr_scheduler_min', type=float, default=0.00001,
+                        help='min LR for `ReduceLROnPlateau` lr decay (default: 1e-5)')
+    parser.add_argument('--max_k', type=int, default=3,
+                        help='Maximum length of node-tuples')
+    parser.add_argument('--num_layers_per_dim', type=int, default=3,
+                        help='number of message passing layers (default: 5)')
+    parser.add_argument('--num_linear_layers', type=int, default=3,
+                        help='number of linear layers before the prediction.')
+    parser.add_argument('--initial_emb_dim', type=int, default=64,
+                        help='dimensionality of hidden units in models (default: 300)')
+    parser.add_argument('--emb_dim', type=int, default=64,
+                        help='dimensionality of hidden units in models (default: 300)')
+    parser.add_argument('--batch_size', type=int, default=32,
+                        help='input batch size for training (default: 32)')
+    parser.add_argument('--epochs', type=int, default=100,
+                        help='number of epochs to train (default: 100)')
+    # parser.add_argument('--num_workers', type=int, default=0,
+    #                     help='number of workers (default: 0)')
+    parser.add_argument('--dataset', type=str, default="PROTEINS",
+                        help='dataset name (default: PROTEINS)')
+
+    # TODO
+    parser.add_argument('--task_type', type=str, default='classification',
+                        help='task type, either (bin)classification, regression or isomorphism (default: classification)')
+
+    # TODO
+    parser.add_argument('--eval_metric', type=str, default='accuracy',
+                        help='evaluation metric (default: accuracy)')
+    # parser.add_argument('--iso_eps', type=int, default=0.01,
+    #                     help='Threshold to define (non-)isomorphism')
+    # parser.add_argument('--minimize', action='store_true',
+    #                     help='whether to minimize evaluation metric or not')
+
+    # TODO
+    parser.add_argument('--result_folder', type=str, default=os.path.join(ROOT_DIR, 'exp', 'results'),
+                        help='filename to output result (default: None, will use `scn/exp/results`)')
+    # parser.add_argument('--exp_name', type=str, default=str(time.time()),
+    #                     help='name for specific experiment; if not provided, a name based on unix timestamp will be '+\
+    #                     'used. (default: None)')
+    parser.add_argument('--folds', type=int, default=10,
+                        help='The number of folds to run on in cross validation experiments')
+    # parser.add_argument('--train_eval_period', type=int, default=10,
+    #                     help='How often to evaluate on train.')
+    # parser.add_argument('--use_edge_features', action='store_true',
+    #                     help="Use edge features for molecular graphs")
+    return parser
