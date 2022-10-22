bf16e27577b0c5dce6395f9934cc3bdad3ba55b4
florian.scheidl@gmail.com <florian.scheidl@gmail.com>
2022-10-22 18:26:27 +0200

    Back to one-hot encoding and manually set number of classes (i.e. this is a hyperparameter). Bug fixes.

--- examples/1-2-3-gnn-class.py
+++ examples/1-2-3-gnn-class.py
@@ -68,7 +68,7 @@ path = osp.join(
 if args.dataset == 'TU_PROTEINS':
     pre_transform = T.Compose([MyPreTransformNoFeatures(num_classes=100)])
 elif args.dataset == 'TU_REDDIT-MULTI-5K':
-    pre_transform = T.Compose([MyPreTransformNoFeatures(num_classes=400)])
+    pre_transform = T.Compose([MyPreTransformNoFeatures(num_classes=600)])
 elif args.dataset == 'TU_IMDB-MULTI':
     pre_transform = T.Compose([MyPreTransformNoFeatures(num_classes=200)])
 else:
